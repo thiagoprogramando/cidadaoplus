cabee65ddba795f84d6a8c41ccfd20c35677ccd6
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2024-03-12 12:27:15 -0300

    fix /create apis
    Consultas
    Autenticação
    Relatórios

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AcessController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('authenticated', [AcessController::class, 'login'])->name('authenticated');
+Route::get('users', [AcessController::class, 'users'])->name('users');
+Route::get('data', [AcessController::class, 'data'])->name('data');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
